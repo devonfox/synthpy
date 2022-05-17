813e9431a49e4c135e88fc5d7f00cb5f303f8982
matt stevenson <stevensonmat2@gmail.com>
2022-05-16 19:02:24 -0700

    update

--- synth.py
+++ synth.py
@@ -1,15 +1,17 @@
 from midi_interface import MidiInterface
 from test_module import TestModule
+from effects import Toaster
 import pyaudio
 
 
 class Synth:
-    def __init__(self, sound_moudle=TestModule()) -> None:
+    def __init__(self, sound_moudle=TestModule(), effect=None) -> None:
         self.p = pyaudio.PyAudio()
         self.volume = 0.5  # range [0.0, 1.0]
         self.fs = 48000  # sampling rate, Hz, must be integer
         self.midi_interface = MidiInterface(self.process_midi)
         self.sound_module = sound_moudle
+        self.effect = effect
         self.note = None
         self.stream = self.p.open(
             format=pyaudio.paFloat32,
@@ -22,13 +24,21 @@ class Synth:
         self.stream.start_stream()
         while True:
             if self.note:
-                self.stream.write(self.volume * self.sound_module.play(self.note))
+                self.stream.write(self.output_data())
 
     def process_midi(self, message):
         msg = message
         if msg.type == "note_on":
+            # self.stream.start_stream()
             self.note = msg.note
         elif msg.type == "note_off":
             if msg.note == self.note:
+                # self.stream.stop_stream()
                 self.note = None
                 print(f"stop")
+
+    def output_data(self):
+        data = self.volume * self.sound_module.play(self.note)
+        if self.effect:
+            data = self.effect.apply_effect(data)
+        return data

a750af18777a99a3f48abf2ecd1b13aeb2b43180
foxdev <foxdev@pdx.edu>
2022-05-24 14:22:29 -0700

    fix duration or chunk

--- test_module.py
+++ test_module.py
@@ -6,9 +6,13 @@ class TestModule:
         print(f"play {note}")
 
         fs = 48000       # sampling rate, Hz, must be integer
-        duration = .3   # in seconds, may be float
         f = 440.0        # sine frequency, Hz, may be float
-        f = (f/32) * (2 ** ((note - 9) / 12))
+        f = (f) * (2 ** ((note - 69) / 12))
+        duration = (2*np.pi)/f # in seconds, may be float
+        # need to adjust this duration to fit the period of the wave, or
+        # maybe a chunk of samples slightly larger might sound better to account for any latency adding to stream
+
+        # possibly change 
 
         samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
         return samples

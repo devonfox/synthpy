cf2ec1bb381f8c33e3076d094dc0c6e3e7602862
foxdev <foxdev@pdx.edu>
2022-05-26 09:12:48 -0700

    moving from pyaudio to sounddevice

--- synth.py
+++ synth.py
@@ -2,20 +2,24 @@ from xml.sax import parseString
 from midi_interface import MidiInterface
 from effects import Toaster
 import pyaudio
+import sounddevice as sd
 import numpy as np
 import queue
-import time
+import sys
 
-chunkdata = queue.Queue(maxsize = 1)
-chunk = 512 # trying to decide on a chunk for audio callback
+chunkdata = queue.Queue(maxsize=1)
+chunk = 512  # trying to decide on a chunk for audio callback
 
+start_idx = 0
 class SineModule:
 
     def play(self, note, s):
         if note:
-            print(f"play {note}: {s}")
+            # print(f"play {note}: {s}")
+            pass
         else:
-            print(f"none: {s}")
+            # print(f"none: {s}")
+            pass
             s += chunk
             zeros = np.zeros(chunk)
             # print(zeros)
@@ -23,14 +27,14 @@ class SineModule:
             return 0
         # fs = 48000       # sampling rate, Hz, must be integer
         f = 440 * 2**((note - 69) / 12)
-        # period = 2 * np.pi * f / fs 
+        # period = 2 * np.pi * f / fs
 
         # do not need a duration, we want to keep creating this as long as the note is held, and
         # then return the full samples, and stream it chunk by chunk to the audio callback, with
         # minimal delay
-        
-        # possibly change 
-        t = np.linspace(s,s + chunk, False).astype(np.float32)
+
+        # possibly change
+        t = np.linspace(s, s + chunk, False).astype(np.float32)
         wave = np.sin(f * t * 2 * np.pi)
         # chunkdata.put(wave)
         # print(chunkdata)
@@ -38,36 +42,42 @@ class SineModule:
         s += chunk
         return s
 
-
-
     def stop(self, note):
         print(f"stop {note}")
-    
+
+
 class Synth:
     def __init__(self, sound_module=SineModule(), effect=None) -> None:
-        self.p = pyaudio.PyAudio()
+        # self.p = pyaudio.PyAudio()
         self.volume = 0.5  # range [0.0, 1.0]
         self.fs = 48000  # sampling rate, Hz, must be integer
         self.midi_interface = MidiInterface(self.process_midi)
         self.sound_module = sound_module
         self.effect = effect
         self.note = None
-        self.stream = self.p.open(
-            format=pyaudio.paFloat32,
-            channels=1,
-            rate=self.fs,
-            output=True,
-            frames_per_buffer=chunk, # 512
-            stream_callback=self.pa_callback
-        )
+        # self.index = 0
+        self.stream = sd.OutputStream(
+            samplerate=self.fs, channels=1, blocksize=1024, callback=sd_callback)
+
+        # self.stream = self.p.open(
+        #     format=pyaudio.paFloat32,
+        #     channels=1,
+        #     rate=self.fs,
+        #     output=True,
+        #     frames_per_buffer=chunk, # 512
+        #     stream_callback=self.pa_callback
 
     def play(self):
-        self.stream.start_stream()
-        # print(self.midi_interface.inport)
-        num = 0
-        while True:
-                num = self.sound_module.play(self.note, num)
+        self.stream.start()
+        try:
+            # print(self.midi_interface.inport)
+            num = 0
+            while True:
+                pass
+                # num = self.sound_module.play(self.note, num)
                 # print(chunkdata)
+        except KeyboardInterrupt:
+            exit()
 
     def process_midi(self, message):
         msg = message
@@ -86,15 +96,14 @@ class Synth:
     #     data = self.volume * self.sound_module.play(self.note)
     #     if self.effect:
     #         data = self.effect.apply_effect(data)
-    #     return data 
+    #     return data
 
-    def pa_callback(self, out_data, frame_count, time_info, status):
-        # print('ok')
-        # print(frame_count)
-        # here take chunks of audio and play in chunks
-        # time.sleep(0.00015)
-        # data = chunkdata.get(block=True)
-        # print(data)
-        data = np.zeros((frame_count,), dtype=np.float32)
-        return (data, pyaudio.paContinue)
-
+def sd_callback(outdata, frame_count, time, status):
+  
+    global start_idx
+    t = (start_idx + np.arange(frame_count)) / 48000
+    t = t.reshape(-1, 1)
+    # print(len(zeros))
+    outdata[:] = 0.2 * np.sin(2 * np.pi * 440.0 * t)
+    print(start_idx)
+    start_idx += frame_count

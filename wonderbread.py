f59cf5b95e166d95ea52dfebf9516cdbd1785ad8
foxdev <foxdev@pdx.edu>
2022-05-28 15:09:58 -0700

    updated to contain arguments

--- wonderbread.py
+++ wonderbread.py
@@ -1,12 +1,18 @@
 from synth import Synth
 import argparse
 
+parser = argparse.ArgumentParser()
 
-# possibly use sounddevice?
+parser.add_argument('--volume', dest='volume', type=float, default=8.0,
+                    help='note volume for the chosen synth module (default: 8.0)')
+parser.add_argument('--buffer', dest='chunk', type=int, default=512,
+                    help='buffer size for continuous audio playback (default: 512)')
+
+arg = parser.parse_args()
 
 
 def main():
-    synth = Synth()
+    synth = Synth(arg)
     synth.play()
 
 

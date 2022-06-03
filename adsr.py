b6f7c4fa3b90c149840357f3ff7c2adfedb7367e
foxdev <foxdev@pdx.edu>
2022-06-02 18:07:49 -0700

    changed attack to use logarithmic increase of amp

--- adsr.py
+++ adsr.py
@@ -1,7 +1,9 @@
+import math
+
 class ADSR:
     def __init__(self) -> None:
         self.fs = 48000
-        self.attack = self.fs * 2
+        self.attack = self.fs * 1
         self.decay = 0.25
         self.sustain = 0.25
         self.release = 0.25
@@ -17,8 +19,14 @@ class ADSR:
 
     def set_release(self, release):
         self.release = release
+    
+    def getAmp(self, vol):
+        exp = (-6 * (10 - vol)) / 20
+        amp = math.pow(10, exp)
+        return amp
 
     def apply_envelope(self, wave_data: list, samples: int) -> list:
+
         if samples < self.attack:
             wave_data = self.apply_attack(wave_data, samples)
 
@@ -26,6 +34,10 @@ class ADSR:
 
     def apply_attack(self, wave_data: list, samples: int) -> list:
         for index, _ in enumerate(wave_data):
-            wave_data[index] *= samples / self.attack
+
+            #  TODO: Fix small click at end of attack 
+            att = 10 * ((samples+index+1)/self.attack)
+            wave_data[index] *= self.getAmp(att)
+            # wave_data[index] *= ((samples+index+1)/self.attack)
 
         return wave_data

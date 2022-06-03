d34594f5f18b5cb9eaa9dbe198424c1ad09c71ee
foxdev <foxdev@pdx.edu>
2022-06-03 15:36:27 -0700

    fixed release - still one bug left if same note

--- sound_module.py
+++ sound_module.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 from adsr import ADSR
+from scipy import signal
 
 class SoundModule:
     def __init__(self, arg) -> None:
@@ -9,12 +10,13 @@ class SoundModule:
         self.arg = arg  # arguments from main
         self.fs = 48000
         self.index = 0
-        self.samples = 1
+        self.samples = 0
         self.asdr = ADSR(arg)
         self.inc = self.arg.chunk / self.fs
+        self.attidx = 0
         self.relidx = self.asdr.release
-        self.lastnote = -1
-        self.endpoint = 0
+        self.relswitch = False
+        # self.endpoint = 0
 
         self.current_note = None
         self.previous_note = None
@@ -27,23 +29,32 @@ class SoundModule:
             self.previous_note = self.current_note
             self.current_note = note
             wave = self.compute_wave()
-            self.lastnote = note
             # reset for new note
             if self.current_note != self.previous_note:
-                self.samples = 1
-                self.endpoint = 0
-                self.relidx = self.asdr.release
+                self.samples = 0
+                # self.endpoint = 0
+                # self.relidx = self.asdr.release
             
             wave = self.asdr.apply_envelope(wave, self.samples)
 
             wave = wave.astype(np.float32)  # converts back to f32 from f64
             self.index += self.inc  # increments current endpoint for sine call
             self.samples += self.arg.chunk
-            self.endpoint = self.samples
+            # self.endpoint = self.samples
+            # if self.samples < self.asdr.attack:
+            #     self.relidx = self.samples
+            # else:
+            #     self.relidx = self.asdr.release
+            # print(self.relidx)
             
         else:
-            print(self.relidx)
-            if self.samples != 1 and self.relidx > 0:
+            
+            # print(self.relidx)
+            if self.samples > 0 and self.relidx >= 0:
+                if self.relswitch is False:
+                    if self.samples < self.asdr.attack:
+                        self.relidx = self.samples
+                    self.relswitch = True
                 wave = self.compute_wave()
                 wave = self.asdr.apply_release(wave, self.relidx)
                 wave = wave.astype(np.float32)  # converts back to f32 from f64
@@ -53,9 +64,10 @@ class SoundModule:
                 
             else:
                 self.index = 0  # starts calc back over for sine
-                self.samples = 1
-                self.endpoint = 0
+                self.samples = 0
+                # self.endpoint = 0
                 self.relidx = self.asdr.release
+                self.relswitch = False
             # sends chunks of silence to play call, sending silence to sounddevice
                 wave = np.zeros(self.arg.chunk).astype(np.float32)
 
@@ -74,8 +86,10 @@ class SoundModule:
             wave = self.square(t, f)
         elif self.wavetype == 'sine':
             wave = self.sine(t, f)
+        elif self.wavetype == 'tri':
+            wave = self.triangle(t, f)
             
-        wave *= self.getAmp(self.arg.volume)  # float calculation
+        wave *= self.getAmp(self.arg.volume)
 
         return wave
     
@@ -86,3 +100,5 @@ class SoundModule:
     def square(self, t, f):
         return 4 * np.floor(f * t) - 2 * np.floor(2*f * t) + 1  # float calculation
         
+    def triangle(self, t, f):
+        return 2 * np.abs(2 * (t/f) - np.floor((t/f) + 0.5)) - 1

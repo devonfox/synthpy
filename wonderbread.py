68c16a95f34c195552f7e72f2c6fffdd9826dba9
foxdev <foxdev@pdx.edu>
2022-06-02 19:18:47 -0700

    added attack argument

--- wonderbread.py
+++ wonderbread.py
@@ -1,3 +1,4 @@
+
 from synth import Synth
 import argparse
 
@@ -7,6 +8,9 @@ parser.add_argument('--volume', dest='volume', type=float, default=8.0,
                     help='note volume for the chosen synth module (default: 8.0)')
 parser.add_argument('--buffer', dest='chunk', type=int, default=512,
                     help='buffer size for continuous audio playback (default: 512)')
+parser.add_argument('--attack', dest='attack', type=float, default=0.25,
+                    help='seconds of attack time (default: 0.25)')
+
 
 arg = parser.parse_args()
 

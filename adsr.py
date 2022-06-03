0c8e3bd656ea68a90fe940d0f53cbfd51854b2a9
foxdev <foxdev@pdx.edu>
2022-06-02 19:17:59 -0700

    changed back to ramp for amp env attack

--- adsr.py
+++ adsr.py
@@ -1,12 +1,13 @@
 import math
 
 class ADSR:
-    def __init__(self) -> None:
+    def __init__(self, arg) -> None:
         self.fs = 48000
-        self.attack = self.fs * 1
+        self.attack = self.fs * arg.attack
         self.decay = 0.25
         self.sustain = 0.25
         self.release = 0.25
+        self.arg = arg
 
     def set_attack(self, attack: float):
         self.attack = attack
@@ -20,14 +21,9 @@ class ADSR:
     def set_release(self, release):
         self.release = release
     
-    def getAmp(self, vol):
-        exp = (-6 * (10 - vol)) / 20
-        amp = math.pow(10, exp)
-        return amp
-
     def apply_envelope(self, wave_data: list, samples: int) -> list:
 
-        if samples < self.attack:
+        if samples <= self.attack:
             wave_data = self.apply_attack(wave_data, samples)
 
         return wave_data
@@ -35,9 +31,6 @@ class ADSR:
     def apply_attack(self, wave_data: list, samples: int) -> list:
         for index, _ in enumerate(wave_data):
 
-            #  TODO: Fix small click at end of attack 
-            att = 10 * ((samples+index+1)/self.attack)
-            wave_data[index] *= self.getAmp(att)
-            # wave_data[index] *= ((samples+index+1)/self.attack)
+            wave_data[index] *= ((samples+index+1)/self.attack)
 
         return wave_data

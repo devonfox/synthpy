1983e7fd8cc79bc4b2dc6b5e972eb05393dc3ab3
foxdev <foxdev@pdx.edu>
2022-06-03 20:39:14 -0700

    updated defaults

--- wonderbread.py
+++ wonderbread.py
@@ -4,13 +4,13 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--volume', dest='volume', type=float, default=8.0,
-                    help='note volume for the chosen synth module (default: 8.0)')
-parser.add_argument('--buffer', dest='chunk', type=int, default=512,
-                    help='buffer size for continuous audio playback (default: 512)')
-parser.add_argument('--attack', dest='attack', type=float, default=0.05,
+parser.add_argument('--volume', dest='volume', type=float, default=7.0,
+                    help='note volume for the chosen synth module (default: 7.0)')
+parser.add_argument('--buffer', dest='chunk', type=int, default=256,
+                    help='buffer size for continuous audio playback (default: 256)')
+parser.add_argument('--attack', dest='attack', type=float, default=0.1,
                     help='seconds of attack time (default: 0.05)')
-parser.add_argument('--release', dest='release', type=float, default=0.2,
+parser.add_argument('--release', dest='release', type=float, default=0.5,
                     help='seconds of release time (default: 0.2)')
 parser.add_argument('--wave', dest='wave', type=str, default='square',
                     help='synth waveform module (default: square)')

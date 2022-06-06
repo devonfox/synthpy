5c8ef1c8df1ff3742e915b515480aa587e670923
foxdev <foxdev@pdx.edu>
2022-06-05 17:06:36 -0700

    added decay and sustain

--- adsr.py
+++ adsr.py
@@ -4,30 +4,27 @@ import math
 class ADSR:
     def __init__(self, arg) -> None:
         self.fs = 48000
-        self.attack = self.fs * arg.attack
-        self.decay = 0.25
-        self.sustain = 0.25
-        self.release = self.fs * arg.release
+        self.attack = self.fs * arg.attack + 0.001
+        self.decay = self.fs * arg.decay
+        self.sustain = arg.sustain + 0.0001
+        self.release = self.fs * arg.release + 0.001
+        self.ddec = self.set_ddec(self.decay)
         self.arg = arg
 
-    def set_attack(self, attack: float):
-        self.attack = attack
-
-    def set_decay(self, decay: float):
-        self.decay = decay
-
-    def set_sustain(self, sustain): # todo Sunday
-        self.sustain = sustain
-
-    def set_release(self, release): # todo Sunday
-        self.release = release
+    def set_ddec(self, decay: float):
+        if decay != 0:
+            return (1 - self.sustain) / decay
+        return 0
 
     def apply_envelope(self, wave_data: list, samples: int, note) -> list:
 
-        if samples < self.attack:
+        if samples <= self.attack:
             wave_data = self.apply_attack(wave_data, samples, note)
-        # add decay
-        # then add sustain
+        elif samples > self.attack and samples < (self.attack + self.decay):
+            wave_data = self.apply_decay(wave_data, note)
+        elif samples > (self.attack + self.decay):
+            wave_data *= self.sustain
+            note.level = self.sustain
         return wave_data
 
     def apply_attack(self, wave_data: list, samples: int, note) -> list:
@@ -38,8 +35,21 @@ class ADSR:
 
         return wave_data
 
+    def apply_decay(self, wave_data: list, note) -> list:
+        for index, _ in enumerate(wave_data):
+            wave_data[index] *= note.declevel
+            note.level = note.declevel
+            note.declevel -= self.ddec
+
+        return wave_data
+
     def apply_release(self, wave_data: list, rel: int) -> list:
         for index, _ in enumerate(wave_data):
             wave_data[index] *= ((rel - index) / self.release)
 
         return wave_data
+
+    def getAmp(self, vol):
+        exp = (-6 * (10 - vol)) / 20
+        amp = math.pow(10, exp)
+        return amp
